5fec26117199cd578159796026cf4a4781bda8f6
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-02-26 12:57:35 +0900

    solve robot-name

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,7 +1,11 @@
 import random
 
 class Robot(object):
-    def __init__(self):
-        pass
-
-
+    def __init__(self, firstLength=2, lastLength=3):
+        random.seed()
+        self.firstName = "".join([chr(random.randint(ord('A'), ord('Z'))) for _ in range(firstLength)])
+        self.lastName = "".join([str(random.randint(0, 9)) for _ in range(lastLength)])
+        self.name = self.firstName + self.lastName
+    
+    def reset(self, firstLength=2, lastLength=3):
+        self.__init__(firstLength, lastLength)
