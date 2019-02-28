7b6e51c20d71f3cd72373ae60916be886d753c29
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-03-01 00:40:00 +0900

    solve acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,17 @@
+def isAlphabet(x):
+    alphabet = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    return x in alphabet
+
 def abbreviate(words):
-    pass
+    words = words.split()
+    acrom = ""
+    for word in words:
+        word = word.upper()
+        if isAlphabet(word[0]):
+            acrom += word[0]
+
+        for i in range(len(word) - 1):
+            if word[i] != '\'' and not isAlphabet(word[i]):
+                acrom += word[i + 1]
+            
+    return acrom
